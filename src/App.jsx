30d72593a916884dde1d6009995386b59eb57817
Kykal <benavides.alan@hotmail.com>
2023-01-10 22:19:49 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 
-//To fetch data
-import axios from 'axios';
-
 
 //Custom components
-import Header from './components/Header';
-import SubjectsNetwork from './components/SubjectsNetwork/SubjectsNetwork';
+import Header						from './components/Header';
+import SubjectsNetwork	from './components/SubjectsNetwork/SubjectsNetwork';
 
 
 //Main component content
